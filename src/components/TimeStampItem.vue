1553ff37b912302409832a2864edee9efd9bf57a
Kirill Aniskin <kir9819@gmail.com>
2020-03-24 22:39:44 +0300

    show minutes, hours, days

--- src/components/TimeStampItem.vue
+++ src/components/TimeStampItem.vue
@@ -2,7 +2,7 @@
 	<div class="time-stamp">
 		<span class="time-stamp-index">{{ item.index }}</span>
 		<div class="time-stamp-description">{{ item.description }}</div>
-		<div class="time-stamp-time">Время: {{ totalTime }}</div>
+		<div class="time-stamp-time">{{ totalTimeString }}</div>
 		<div class="time-stamp-status">{{ isRunning ? '⏸️' : '▶️' }}</div>
 	</div>
 </template>
@@ -15,13 +15,35 @@ import { TimeStamps } from 'Utils/ts/TimeStamps' // eslint-disable-line
 export default class TimeStampItem extends Vue {
 	@Prop(Object) private item!: TimeStamps
 
-	get totalTime(): string {
+	get totalTime(): number {
 		const totalSeconds = this.item.timeStamps.reduce((total, current) => {
 			total += current.end.getTime() - current.start.getTime() // eslint-disable-line
 			return total
 		}, 0) / 1000
 
-		return totalSeconds.toFixed(0).toLocaleString()
+		return totalSeconds
+	}
+
+	get totalTimeString(): string {
+		let { totalTime } = this
+
+		const days = Math.floor(totalTime / 86400)
+		totalTime -= days * 86400
+
+		const hours = Math.floor(totalTime / 3600)
+		totalTime -= hours * 3600
+
+		const min = Math.floor(totalTime / 60)
+		totalTime -= min * 60
+
+		const sec = Math.floor(totalTime)
+
+		const daysString = days ? `${days} д. ` : ''
+		const hoursString = `${hours.toString().padStart(2, '0')}`
+		const minString = `:${min.toString().padStart(2, '0')}`
+		const secString = `:${sec.toString().padStart(2, '0')}`
+
+		return daysString + hoursString + minString + secString
 	}
 
 	get isRunning(): boolean {
