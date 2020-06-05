8c4741a8f86c97349f8278234a2247dca5910889
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-05 22:28:51 +0000

    Deploy kir9819/time-manager to github.com/kir9819/time-manager.git:gh-pages

---
+++ precache-manifest.241c1c7c83458a05e07b7b3a3033680d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31ac8eb05399c31d7dcd",
+    "url": "/time-manager/css/app.467968d7.css"
+  },
+  {
+    "revision": "6cc069a9aab9103099fd",
+    "url": "/time-manager/css/chunk-10302cb9.71e0edfb.css"
+  },
+  {
+    "revision": "04be5d061f4d7364b57e",
+    "url": "/time-manager/css/chunk-3b323e23.9b3b67f6.css"
+  },
+  {
+    "revision": "e3b221743a33d7f51c6d",
+    "url": "/time-manager/css/chunk-3c371ce4.8cb59e2d.css"
+  },
+  {
+    "revision": "a1868900b034f1c5d3fe",
+    "url": "/time-manager/css/chunk-59799dcb.3094b2a3.css"
+  },
+  {
+    "revision": "aeadf972f9af10d63a97779b24ffac22",
+    "url": "/time-manager/index.html"
+  },
+  {
+    "revision": "31ac8eb05399c31d7dcd",
+    "url": "/time-manager/js/app.8955e970.js"
+  },
+  {
+    "revision": "6cc069a9aab9103099fd",
+    "url": "/time-manager/js/chunk-10302cb9.2f5e8271.js"
+  },
+  {
+    "revision": "8089ae84e74a969d31b5",
+    "url": "/time-manager/js/chunk-2d9cc065.537cb430.js"
+  },
+  {
+    "revision": "04be5d061f4d7364b57e",
+    "url": "/time-manager/js/chunk-3b323e23.adce5c49.js"
+  },
+  {
+    "revision": "e3b221743a33d7f51c6d",
+    "url": "/time-manager/js/chunk-3c371ce4.e70d2e6d.js"
+  },
+  {
+    "revision": "a1868900b034f1c5d3fe",
+    "url": "/time-manager/js/chunk-59799dcb.a62142be.js"
+  },
+  {
+    "revision": "e19a6e9340775692f440",
+    "url": "/time-manager/js/chunk-vendors.74fc0d51.js"
+  },
+  {
+    "revision": "dcff03d44f07c6f7cc0d79f60c3419c6",
+    "url": "/time-manager/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/time-manager/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/time-manager/precache-manifest.241c1c7c83458a05e07b7b3a3033680d.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "time-manager"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
